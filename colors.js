c8ec7dd8a8433260ee726f5c7aa7743bd903152d
psch300 <43133025+psch300@users.noreply.github.com>
2020-04-01 20:46:48 +0900

    javascript 복습 완료

---
+++ colors.js
@@ -0,0 +1,38 @@
+var Body = {
+  setColor : function (color) {
+    // document.querySelector('body').style.color = color
+    $('body').css('color', color);
+  },
+  setBackgroundColor : (color) => {
+    // document.querySelector('body').style.backgroundColor = color
+    $('body').css('backgroundColor', color);
+  }
+}
+
+var Links = {
+  setColor : function (color) {
+    // var links = document.querySelectorAll('a');
+    // for (let i=0; i<links.length; i++) {
+    //   links[i].style.color = color;
+    // }
+
+    $('a').css('color', color);
+  }
+}
+
+function nightDayHandler(self) {
+  var target = document.querySelector('body');
+  if (self.value === 'night') {
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+
+    Links.setColor('powderblue');
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+
+    Links.setColor('black');
+  }
+}
